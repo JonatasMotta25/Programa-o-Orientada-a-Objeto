2b4c04a19ceac2b49cc6d74c3a19b7772ce2bd55
Jonatas Motta da Silva <jonatasmotta25@gmail.com>
2025-05-30 20:01:42 -0300

    Add files via upload

---
+++ revisaop2.py
@@ -0,0 +1,15 @@
+class Loja:
+    def __init__(self, produto, valor):
+        self.produto = produto 
+        self.valor = valor
+        self.catalago = []
+
+    def realizar_venda():
+
+    def oferecer_promocao():
+
+    def adicionar_produto(produto,valor):
+
+    def remover_produto(produto):
+
+    def listar_produtos():
